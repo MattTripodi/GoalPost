4d400c9dca8577418455303172d28d12a7f41660
Matt Tripodi <mtripodi1027@gmail.com>
2017-09-25 12:58:31 -0400

    Created createGoalVC UI

---
+++ GoalPost/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Matt Tripodi on 9/25/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+	case longTerm = "Long Term"
+	case shortTerm = "Short Term"
+}
---
+++ GoalPost/Extensions/UIViewControllerExt.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewControllerExt.swift
+//  GoalPost
+//
+//  Created by Matt Tripodi on 9/25/17.
+//  Copyright © 2017 Matthew Tripodi. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+	
+}
