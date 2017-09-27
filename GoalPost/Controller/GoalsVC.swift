3bcbee2aa9ccc8cc0968667ebaaffdfeba8ee16e
Matt Tripodi <mtripodi1027@gmail.com>
2017-09-26 23:21:21 -0400

    Added and completed Undo functionality to undo the most recent deleted goal

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -15,6 +15,8 @@ class GoalsVC: UIViewController {
 	
 	// Outlets
 	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var undoView: UIView!
+	@IBOutlet weak var undoBtn: UIButton!
 	
 	// Variables
 	var goals: [Goal] = []
@@ -23,7 +25,9 @@ class GoalsVC: UIViewController {
 		super.viewDidLoad()
 		tableView.delegate = self
 		tableView.dataSource = self
-		tableView.isHidden = false 
+		tableView.isHidden = false
+		undoView.alpha = 0.0
+		undoBtn.isHidden = true
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
@@ -47,6 +51,14 @@ class GoalsVC: UIViewController {
 	@IBAction func addGoalBtnWasPressed(_ sender: Any) {
 		guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
 		presentDetail(createGoalVC)
+	}
+	
+	@IBAction func undoBtnWasPressed(_ sender: Any) {
+		undoView.alpha = 0.0
+		guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+		managedContext.undoManager?.undo()
+		fetchCoreDataObjects()
+		tableView.reloadData()
 	}
 	
 }
@@ -83,20 +95,71 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
 			tableView.deleteRows(at: [indexPath], with: .automatic)
 		}
 		
+		let addAction = UITableViewRowAction(style: .normal, title: "ADD 1") { (rowAction, indexPath) in
+			self.setProgress(atIndexPath: indexPath)
+			tableView.reloadRows(at: [indexPath], with: .automatic)
+		}
+		
 		deleteAction.backgroundColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
-		return [deleteAction]
+		addAction.backgroundColor = #colorLiteral(red: 0.9176470588, green: 0.662745098, blue: 0.2666666667, alpha: 1)
+		
+		let goal = goals[indexPath.row]
+		
+		if goal.goalProgress == goal.goalCompletionValue {
+			return [deleteAction]
+		} else {
+			return [deleteAction, addAction]
+		}
 	}
 }
 
 extension GoalsVC {
+	
+	func fadeOutView(view: UIView) {
+		
+		UIView.animate(withDuration: 1.0, delay: 1.5, options: .curveEaseOut, animations: {
+			view.alpha = 0.0
+		}, completion: { (true) in
+			self.undoBtn.isHidden = true
+		})
+	}
+	
+	func fadeInView(view: UIView) {
+		UIView.animate(withDuration: 3.0) {
+			view.alpha = 1.0
+		}
+	}
+	
+	func setProgress(atIndexPath indexPath: IndexPath) {
+		guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+		
+		let chosenGoal = goals[indexPath.row]
+		
+		if chosenGoal.goalProgress < chosenGoal.goalCompletionValue {
+			chosenGoal.goalProgress = chosenGoal.goalProgress + 1
+		} else {
+			return
+		}
+		
+		do {
+			try managedContext.save()
+			print("Successfully set progress!")
+		} catch {
+			debugPrint("Could not set progress: \(error.localizedDescription)")
+		}
+	}
+	
 	func removeGoal(atIndexPath indexPath: IndexPath) {
 		guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
 		
+		managedContext.undoManager = UndoManager()
 		managedContext.delete(goals[indexPath.row])
 		
 		do {
 			try managedContext.save()
-			print("Successfully removed goal!")
+			undoBtn.isHidden = false
+			fadeInView(view: undoView)
+			fadeOutView(view: undoView)
 		} catch {
 			debugPrint("Could not remove: \(error.localizedDescription)")
 		}
